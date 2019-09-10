559800200150c2b882dad83cde420588a1d5df66
David Himmelstrup <david.himmelstrup@sc.com>
2019-09-10 09:38:07 +0800

    Reduce output during testing.
    Former-commit-id: 69966bd73d6aec62ce7a9dc141ef7d31cb8087fd

--- test/UnitTests.hs
+++ test/UnitTests.hs
@@ -16,7 +16,7 @@ unitTestFolder :: FilePath -> IO TestTree
 unitTestFolder path = do
   files <- getDirectoryContents path
   return $ testGroup "examples"
-    [ goldenVsStringDiff file (\ref new -> ["diff", ref, new]) fullPath (genGolden hsPath)
+    [ goldenVsStringDiff file (\ref new -> ["diff", "--brief", ref, new]) fullPath (genGolden hsPath)
     | file <- files
     , let fullPath = path </> file
           hsPath = replaceExtension fullPath "hs"
