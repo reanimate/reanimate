a837ea2abc8f84826fe89dc0953d5c60632defa7
David Himmelstrup <lemmih@gmail.com>
2019-11-28 20:45:13 +0800

    Add blender support.

---
+++ src/Reanimate/Blender.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Reanimate.Blender
+  ( blender
+  , blender'
+  ) where
+
+import           Codec.Picture.Png
+import qualified Data.ByteString            as B
+import           Data.Text                  (Text)
+-- import qualified Data.Text                  as T
+import qualified Data.Text.IO               as T
+import           Graphics.SvgTree           (Tree (..))
+import           Reanimate.Cache
+import           Reanimate.Misc
+import           Reanimate.Raster
+import           Reanimate.Svg.Constructors
+import           System.FilePath            (replaceExtension, (<.>))
+import           System.IO.Unsafe           (unsafePerformIO)
+import           Data.Hashable
+
+blender :: Text -> Tree
+blender script =
+  (unsafePerformIO $ mkBlenderImage script)
+
+blender' :: Text -> FilePath
+blender' script =
+  (unsafePerformIO $ mkBlenderImage' script)
+
+mkBlenderImage :: Text -> IO Tree
+mkBlenderImage script = do
+    png <- B.readFile =<< mkBlenderImage' script
+    case decodePng png of
+      Left{}    -> error "bad image"
+      Right img -> return $ center $ scaleToSize 16 9 $ embedDynamicImage img
+
+mkBlenderImage' :: Text -> IO FilePath
+mkBlenderImage' script = cacheFile template $ \target -> do
+    exec <- requireExecutable "blender"
+    let py_file = replaceExtension target "py"
+    T.writeFile py_file script
+    runCmd exec [ "--background","--render-format", "PNG"
+                , "--render-output", target, "--python", py_file]
+  where
+    template = show (hash script) <.> "png"
