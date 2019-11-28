d9dff8dd243af9c4d976e73d9d4c30fcd278a7f3
David Himmelstrup <lemmih@gmail.com>
2019-11-28 21:04:49 +0800

    Improve blender example.

---
+++ examples/blender_default_cube.hs
@@ -0,0 +1,41 @@
+#!/usr/bin/env stack
+-- stack runghc --package reanimate
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+module Main (main) where
+
+import           Data.String.Here
+import           Reanimate
+import           Reanimate.Blender
+
+main :: IO ()
+main = reanimate $ mkAnimation 5 $ \t ->
+  let s = t * pi in
+  mkGroup
+  [ mkBackground "black"
+  , blender (script s)
+  , withFillColor "white" $
+    translate (-4.5) 2 $
+    scale 0.1 $
+    flipYAxis $
+    mkText "default cube"
+  ]
+  where
+    script s = [iTrim|
+import bpy
+
+if __name__ == "__main__":
+    # Args
+    resolution_percentage = 40
+
+    # Setting
+    default_scene = bpy.context.scene
+    default_scene.render.resolution_percentage = resolution_percentage
+    default_scene.render.film_transparent = True
+
+    default_cube = bpy.data.objects["Cube"]
+    default_cube.rotation_euler = (0,0,${s})
+
+    # Rendering
+    bpy.ops.render.render(animation=False, write_still=True)
+|]
