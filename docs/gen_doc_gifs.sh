405fe8d280467496cc280001a58d4100c9f856f5
David Himmelstrup <lemmih@gmail.com>
2019-11-29 09:35:30 +0800

    Fix interpolation gifs.
    Former-commit-id: 1d596ff95e002b77fad2c5954e63846d252abf43

--- docs/gen_doc_gifs.sh
+++ docs/gen_doc_gifs.sh
@@ -2,7 +2,8 @@
 ROOT=`stack path --project-root`
 
 TINY='^(doc_turbo|doc_viridis|doc_magma|doc_inferno|doc_plasma|doc_sinebow|doc_cividis|doc_jet|
-        doc_hsv|doc_hsvMatlab|doc_greyscale|doc_parula|doc_rgbComponents|doc_hsvComponents)$'
+        newline|doc_hsvMatlab|doc_greyscale|doc_parula|doc_rgbComponents|doc_hsvComponents|
+        newline|doc_lchComponents|doc_xyzComponents|doc_labComponents)$'
 
 for src in $ROOT/examples/doc_*.hs; do
   BASE=`basename $src .hs`
