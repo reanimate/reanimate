68a98deff1ec9d3eb3a8988e90573f4e6551f062
David Himmelstrup <lemmih@gmail.com>
2019-12-08 19:46:54 +0800

    Smooth out blender example.
    Former-commit-id: 7307b9db36f940e7d61927eb4de20bb418ca1688

--- examples/tut_glue_blender.hs
+++ examples/tut_glue_blender.hs
@@ -2,20 +2,49 @@
 -- stack runghc --package reanimate
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE BangPatterns       #-}
 module Main (main) where
 
+import           Codec.Picture
 import           Data.String.Here
 import qualified Data.Text         as T
 import           Reanimate
 import           Reanimate.Blender
 import           Reanimate.Raster
+import           Reanimate.Scene
+import Graphics.SvgTree
+import System.IO.Unsafe
+import qualified Data.Text.IO as T
 
 main :: IO ()
-main = reanimate $ mkAnimation 1 $ \t ->
-    mkGroup
-    [ mkBackground "black"
-    , blender (script t)
-    ]
+main = seq texture $ reanimate $ parA bg $ sceneAnimation $ do
+    bend <- newVar 0
+    trans <- newVar 0
+    rotX <- newVar 0
+    rotY <- newVar 0
+    _ <- newSprite $ do
+      getBend <- freezeVar bend
+      getTrans <- freezeVar trans
+      getRotX <- freezeVar rotX
+      getRotY <- freezeVar rotY
+      return $ \real_t dur t ->
+        blender (script (getBend real_t) (getTrans real_t) (getRotX real_t) (getRotY real_t))
+    tweenVar trans 5 (\t v -> fromToS v (-2) $ curveS 2 (t/5))
+    tweenVar bend 5 (\t v -> fromToS v 1 $ curveS 2 (t/5))
+    tweenVar rotY 15 (\t v -> fromToS v (pi*2*2) $ curveS 2 (t/15))
+    fork $ do
+      tweenVar rotX 5 (\t v -> fromToS v (-pi/5) $ curveS 2 (t/5))
+      wait 5
+      tweenVar rotX 5 (\t v -> fromToS v (pi/5) $ curveS 2 (t/5))
+    wait (15-5)
+    tweenVar bend 5 (\t v -> fromToS 1 0 $ curveS 2 (t/5))
+    tweenVar rotX 5 (\t v -> fromToS v 0 $ curveS 2 (t/5))
+    tweenVar trans 5 (\t v -> fromToS v 0 $ curveS 2 (t/5))
+    wait 4
+    -- tweenVar trans 1 (\t v -> fromToS v 0 $ curveS 2 t)
+    wait 1
+  where
+    bg = animate $ const $ mkBackground "grey"
 
 texture :: FilePath
 texture = svgAsPngFile (mkGroup
@@ -24,41 +53,35 @@ texture = svgAsPngFile (mkGroup
     scale 2 $ center $
     latexAlign "\\sum_{k=1}^\\infty {1 \\over k^2} = {\\pi^2 \\over 6}"])
 
-script :: Double -> T.Text
-script s = [iTrim|
+script :: Double -> Double -> Double -> Double -> T.Text
+script bend transZ rotX rotY = [iTrim|
 import os
 import math
 
 import bpy
 
 cam = bpy.data.objects['Camera']
-cam.location = (0,0,22.25)
+cam.location = (0,0,22.25 + ${transZ})
 cam.rotation_euler = (0, 0, 0)
-bpy.ops.object.empty_add(location=(0.0, 0, -2.0))
+bpy.ops.object.empty_add(location=(0.0, 0, 0))
 focus_target = bpy.context.object
 bpy.ops.object.select_all(action='DESELECT')
 cam.select_set(True)
 focus_target.select_set(True)
 bpy.ops.object.parent_set()
 
-focus_target.rotation_euler = (${fromToS 0 (-pi/3) s}, 0, 0)
-
+focus_target.rotation_euler = (${rotX}, 0, 0)
 
 
 origin = bpy.data.objects['Cube']
-
 bpy.ops.object.select_all(action='DESELECT')
 origin.select_set(True)
 bpy.ops.object.delete()
 
-#bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=4)
-#bpy.ops.mesh.primitive_uv_sphere_add()
-x = ${s}
+x = ${bend}
 bpy.ops.mesh.primitive_plane_add()
 plane = bpy.context.object
-plane.scale = (1,1.778,1)
-plane.scale = (16/2,9/2,1)
-plane.scale = (16/2,${fromToS (9/2) 4 s},1)
+plane.scale = (16/2,${fromToS (9/2) 4 bend},1)
 bpy.ops.object.shade_smooth()
 
 bpy.context.object.active_material = bpy.data.materials['Material']
@@ -72,8 +95,8 @@ image_node.image = bpy.data.images.load('${T.pack texture}')
 
 
 modifier = plane.modifiers.new(name='Subsurf', type='SUBSURF')
-modifier.levels = 5
-modifier.render_levels = 5
+modifier.levels = 7
+modifier.render_levels = 7
 modifier.subdivision_type = 'SIMPLE'
 
 bpy.ops.object.empty_add(type='ARROWS',rotation=(math.pi/2,0,0))
@@ -85,11 +108,24 @@ bendUp.origin = empty
 bendUp.deform_axis = 'X'
 bendUp.factor = -math.pi*x
 
-bendAround = plane.modifiers.new(name='Bend up', type='SIMPLE_DEFORM')
+bendAround = plane.modifiers.new(name='Bend around', type='SIMPLE_DEFORM')
 bendAround.deform_method = 'BEND'
 bendAround.origin = empty
 bendAround.deform_axis = 'Z'
 bendAround.factor = -math.pi*2*x
+
+bpy.context.view_layer.objects.active = plane
+print(bpy.ops.object.modifier_apply(modifier='Subsurf'))
+print(bpy.ops.object.modifier_apply(modifier='Bend up'))
+print(bpy.ops.object.modifier_apply(modifier='Bend around'))
+
+bpy.ops.object.select_all(action='DESELECT')
+plane.select_set(True);
+#bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
+bpy.ops.object.origin_clear()
+bpy.ops.object.origin_set(type='GEOMETRY_ORIGIN')
+
+plane.rotation_euler = (0, ${rotY}, 0)
 
 scn = bpy.context.scene
 # scn.render.engine = 'CYCLES'
