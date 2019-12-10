37c044e873514d3a81d6f51c8cc6c7105124b971
Jan Hrček <honza.hrk@gmail.com>
2019-12-10 17:37:25 +0100

    Reuse Reanimate.Builtin.Documentation.showColorMap

--- examples/doc_viridis.hs
+++ examples/doc_viridis.hs
@@ -7,12 +7,4 @@ import Reanimate
 import Reanimate.Builtin.Documentation
 
 main :: IO ()
-main = reanimate $ docEnv $ animate $ const $ mkColorMap viridis
-
-mkColorMap :: (Double -> PixelRGB8) -> SVG
-mkColorMap f = center $ scaleToSize screenWidth screenHeight $ embedImage img
-  where
-    width = 256
-    height = 1
-    img = generateImage pixelRenderer width height
-    pixelRenderer x _y = f (fromIntegral x / fromIntegral (width-1))
+main = reanimate $ docEnv $ animate $ const $ showColorMap viridis
