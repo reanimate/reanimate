5a7c53c3f4ab10ada8ba6a8cd44f56f0e2a075cd
Jan Hrček <honza.hrk@gmail.com>
2019-12-14 17:00:03 +0100

    Implement basic elm viewer
    Former-commit-id: bb096343c7e818db5ac668c5666e6935c1172916

--- src/Reanimate/Driver/Server.hs
+++ src/Reanimate/Driver/Server.hs
@@ -9,15 +9,19 @@ import           Control.Concurrent.MVar
 import           Control.Exception       (SomeException, catch, finally)
 import           Control.Monad
 import           Control.Monad.Fix       (fix)
+import           Data.Hashable           (hash)
 import           Data.Text               (Text)
 import qualified Data.Text               as T
+import qualified Data.Text.IO            as T
 import qualified Data.Text.Read          as T
 import           GHC.Environment         (getFullArgs)
 import           Network.WebSockets
 import           Paths_reanimate
 import           Reanimate.Misc          (runCmdLazy, runCmd_)
-import           System.Directory        (doesFileExist, findFile,
+import           System.Directory        (createDirectoryIfMissing,
+                                          doesFileExist, findFile,
                                           listDirectory, makeAbsolute,
+                                          removeDirectoryRecursive,
                                           withCurrentDirectory)
 import           System.Environment      (getProgName)
 import           System.Exit
@@ -52,61 +56,64 @@ serve = withManager $ \watch -> do
       then do
         putStrLn "Already connected to browser. Rejecting."
         rejectRequestWith pending defaultRejectRequest
-      else do
+      else withSystemTempDirectory "reanimate-svgs" $ \tmpDir -> do
+        createDirectoryIfMissing True tmpDir
         conn <- acceptRequest pending
         slave <- newEmptyMVar
         let handler = modifyMVar_ slave $ \tid -> do
               putStrLn "Reloading code..."
               killThread tid
-              forkIO $ ignoreErrors $ slaveHandler conn self
+              forkIO $ ignoreErrors $ slaveHandler conn self tmpDir
             killSlave = do
               tid <- takeMVar slave
               killThread tid
         stop <- watchFile watch self handler
         putMVar slave =<< forkIO (return ())
+        handler
         let loop = do
-              -- FIXME: We don't use fps here.
-              _fps <- receiveData conn :: IO T.Text
+              -- FIXME: We don't use msg here.
+              _msg <- receiveData conn :: IO T.Text
               handler
               loop
-        loop `finally` (swapMVar hasConnectionVar False >> stop >> killSlave)
+        loop `finally` (removeDirectoryRecursive tmpDir >> swapMVar hasConnectionVar False >> stop >> killSlave)
 
 ignoreErrors :: IO () -> IO ()
 ignoreErrors action = action `catch` \(_::SomeException) -> return ()
 
 openViewer :: IO ()
 openViewer = do
-  url <- getDataFileName "viewer/build/index.html"
+  url <- getDataFileName "viewer-elm/dist/index.html"
   putStrLn "Opening browser..."
   bSucc <- openBrowser url
   if bSucc
       then putStrLn "Browser opened."
       else hPutStrLn stderr $ "Failed to open browser. Manually visit: " ++ url
 
-slaveHandler :: Connection -> FilePath -> IO ()
-slaveHandler conn self =
+slaveHandler :: Connection -> FilePath -> FilePath -> IO ()
+slaveHandler conn self svgDir =
   withCurrentDirectory (takeDirectory self) $
   withSystemTempDirectory "reanimate" $ \tmpDir ->
   withTempFile tmpDir "reanimate.exe" $ \tmpExecutable handle -> do
     hClose handle
-    sendTextData conn (T.pack "Compiling")
+    sendTextData conn (T.pack "status\nCompiling")
     ret <- runCmd_ "stack" $ ["ghc", "--"] ++ ghcOptions tmpDir ++ [takeFileName self, "-o", tmpExecutable]
     case ret of
       Left err ->
-        sendTextData conn $ T.pack $ "Error" ++ unlines (drop 3 (lines err))
+        sendTextData conn $ T.pack $ "error\n" ++ unlines (drop 3 (lines err))
       Right{} -> runCmdLazy tmpExecutable execOpts $ \getFrame -> do
         (frameCount,_) <- expectFrame =<< getFrame
-        sendTextData conn (T.pack $ show frameCount)
+        sendTextData conn (T.pack $ "frame_count\n" ++ show frameCount)
         fix $ \loop -> do
           (frameIdx, frame) <- expectFrame =<< getFrame
-          sendTextData conn (T.pack $ show frameIdx)
-          sendTextData conn frame
+          let fileName = svgDir </> show (hash frame) <.> "svg"
+          T.writeFile fileName frame
+          sendTextData conn (T.pack $ "frame\n" ++ show frameIdx ++ "\n" ++ fileName)
           loop
   where
     execOpts = ["raw", "+RTS", "-N", "-M1G", "-RTS"]
     expectFrame :: Either String Text -> IO (Integer, Text)
     expectFrame (Left "") = do
-      sendTextData conn (T.pack "Done")
+      sendTextData conn (T.pack "status\nDone")
       exitSuccess
     expectFrame (Left err) = do
       sendTextData conn $ T.pack $ "Error" ++ err
@@ -116,7 +123,7 @@ slaveHandler conn self =
         Left err -> do
           hPutStrLn stderr (T.unpack frame)
           hPutStrLn stderr $ "expectFrame: " ++ err
-          sendTextData conn $ T.pack $ "Error" ++ err
+          sendTextData conn $ T.pack $ "error\n" ++ err
           exitWith (ExitFailure 1)
         Right (frameNumber, rest) ->
           pure (frameNumber, rest)
