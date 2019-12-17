cefdfc4e473679d641f423601a306124d3998d53
David Himmelstrup <lemmih@gmail.com>
2019-12-17 20:43:41 +0800

    Fix bug in xyzComponents documentation.

--- examples/doc_xyzComponents.hs
+++ examples/doc_xyzComponents.hs
@@ -8,7 +8,7 @@ import Reanimate.Builtin.Documentation
 import Codec.Picture
 
 main :: IO ()
-main = reanimate $ docEnv $ animate $ const $ showColorMap $ interpolateRGB8 rgbComponents yellow blue
+main = reanimate $ docEnv $ animate $ const $ showColorMap $ interpolateRGB8 xyzComponents yellow blue
   where
     yellow = PixelRGB8 0xFF 0xFF 0x00
     blue = PixelRGB8 0x00 0x00 0xFF
