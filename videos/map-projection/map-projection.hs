8ccdc2607bf43e20b8cf2d9aaefbdf1ed934efd3
David Himmelstrup <lemmih@gmail.com>
2020-02-05 16:28:13 +0800

    Refactor projections.

--- videos/map-projection/map-projection.hs
+++ videos/map-projection/map-projection.hs
@@ -11,9 +11,17 @@ import Debug.Trace
 
 main :: IO ()
 main = seq equirectangular $ reanimate $ playThenReverseA $ sceneAnimation $ do
-    play $ setDuration 1 $ animate $ \t ->
+    play $ setDuration 5 $ animate $ \t ->
       scaleToSize screenWidth screenHeight $
-      embedImage (bottomley $ fromToS (toRads 1) (toRads 30) $ curveS 2 t) -- (mercator t)
+      --embedImage (bottomley $ fromToS (toRads 1) (toRads 30) $ curveS 2 t) -- (mercator t)
+      --embedImage (mollweide $ curveS 2 t)
+      --embedImage (mercator 1)
+      --embedImage (project mollweideP)
+      -- embedImage (project $ mergeP mollweideP (bottomleyP (toRads 30)) $ curveS 2 t)
+      --embedImage (project $ bottomleyP $ fromToS (toRads 0.1) (toRads 30) $ curveS 2 t)
+      --embedImage (project $ mergeP mollweideP lambertP $ curveS 2 t)
+      --embedImage (project $ mergeP mollweideP sinusoidalP $ curveS 2 t)
+      embedImage (project $ mergeP equirectangularP mollweideP $ curveS 2 t)
 
 equirectangular :: Image PixelRGB8
 equirectangular = unsafePerformIO $ do
@@ -22,18 +30,47 @@ equirectangular = unsafePerformIO $ do
     Left err  -> error err
     Right img -> return $ convertRGB8 img
 
-mercator :: Double -> Image PixelRGB8
-mercator t =
-    generateImage fn w h
+-- x y -> (lon, lat)
+-- [0,1] [0,1] -> [-pi,+pi] [-pi/2, +pi/2]
+type Projection = Double -> Double -> (Double, Double)
+
+project :: Projection -> Image PixelRGB8
+project p = generateImage fn w h
   where
     w = imageWidth equirectangular
     h = imageHeight equirectangular
     fn xPx yPx =
-      let -- x = fromToS (-pi) pi (fromIntegral xPx / fromIntegral (w-1))
-          y = fromToS (-pi) (pi) (fromIntegral yPx / fromIntegral (h-1))
-          orig_y = (fromToS 0 (fromIntegral h) ((atan (sinh y) + (pi/2)) / pi))
-          mid = fromToS (fromIntegral yPx) orig_y t
-      in pixelAt equirectangular xPx (round mid)
+      let x = (fromIntegral xPx / fromIntegral (w-1))
+          y = (fromIntegral yPx / fromIntegral (h-1))
+          (lon, lat) = p x y
+          x_e = round $ fromToS 0 (fromIntegral w-1) ((lon+pi) / (2*pi))
+          y_e = round $ fromToS 0 (fromIntegral h-1) ((lat+pi/2) / pi)
+      in
+      if x_e < 0 || x_e >= w || y_e < 0 || y_e >= h
+        then PixelRGB8 0 0 0
+        else pixelAt equirectangular x_e y_e
+
+mergeP :: Projection -> Projection -> Double -> Projection
+mergeP p1 p2 t = \x y ->
+    let (lon1, lat1) = p1 x y
+        (lon2, lat2) = p2 x y
+    in if oob lon1 lat1 && oob lon2 lat2
+      then (100, 100)
+      else (fromToS lon1 lon2 t, fromToS lat1 lat2 t)
+  where
+    oob lon lat = lon < (-pi) || lon > pi || lat < (-pi/2) || lat > pi/2
+
+equirectangularP :: Projection
+equirectangularP x y = (xPi, yPi)
+  where
+    xPi = fromToS (-pi) pi x
+    yPi = fromToS (-pi/2) (pi/2) y
+
+mercatorP :: Projection
+mercatorP x y = (xPi, atan (sinh yPi))
+  where
+    xPi = fromToS (-pi) pi x
+    yPi = fromToS (-pi) pi y
 
 {- Mollweide
 phi = latitude
@@ -46,40 +83,14 @@ x [-2sqrt(2), +2sqrt(2)]
 y [-sqrt(2), +sqrt(2)]
 
 -}
-mollweide :: Double -> Image PixelRGB8
-mollweide t =
-    generateImage fn w h
+mollweideP :: Projection
+mollweideP x' y' = (lam, phi)
   where
-    w = imageWidth equirectangular
-    h = imageHeight equirectangular
-    fn xPx yPx = -- trace (show (xPx, yPx, lam, phi, x_rect, y_rect)) $
-        if x_new < 0 || x_new >= w || y_new < 0 || y_new >= h -- lam < -pi || lam > pi
-          then PixelRGB8 0 0 0
-          else pixelAt equirectangular x_new y_new
-      where
-        x = fromToS (-2*sqrt(2)) (2*sqrt(2)) (fromIntegral xPx / fromIntegral (w-1))
-        y = fromToS (-sqrt 2) (sqrt 2) (fromIntegral yPx / fromIntegral (h-1))
-        theta = asin (y/sqrt(2))
-        lam = (pi*x/(2*sqrt(2)*cos theta))
-        phi = asin ((2*theta+sin(2*theta))/pi)
-        x_rect = (((lam + pi) / (2*pi))*fromIntegral w)
-        y_rect = (((phi + pi/2) / (pi))*fromIntegral h)
-        x_new = round (fromToS (fromIntegral xPx) x_rect t)
-        y_new = round (fromToS (fromIntegral yPx) y_rect t)
-    -- lam = fromToS (-pi) pi (fromIntegral xPx / fromIntegral (w-1))
-    -- phi = fromToS (-pi/2) (pi/2) (fromIntegral yPx / fromIntegral (h-1))
-    -- theta = theta_gen phi 10
-    -- x_rect = (2*sqrt(2))/pi * lam * cos theta
-    -- y_rect = sqrt(2)*sin theta
-
-theta_gen phi | phi == pi/2 = const (pi/2)
-theta_gen phi | phi == -pi/2 = const (-pi/2)
-theta_gen phi = worker
-  where
-    worker 0 = phi
-    worker n =
-      let sub = worker (n-1) in
-      sub - (2*sub + sin (2*sub) - pi*sin phi) / (2+2*cos (2*sub))
+    x = fromToS (-2*sqrt(2)) (2*sqrt(2)) x'
+    y = fromToS (-sqrt 2) (sqrt 2) y'
+    theta = asin (y/sqrt(2))
+    lam = pi*x/(2*sqrt(2)*cos theta)
+    phi = asin ((2*theta+sin(2*theta))/pi)
 
 
 {- Lambert
@@ -87,68 +98,48 @@ x = lam
 y = sin phi
 -}
 
-lambert :: Double -> Image PixelRGB8
-lambert t =
-    generateImage fn w h
+lambertP :: Projection
+lambertP x' y' = (x, asin y)
   where
-    w = imageWidth equirectangular
-    h = imageHeight equirectangular
-    fn xPx yPx = -- trace (show (xPx, yPx, lam, phi, x_rect, y_rect)) $
-        if x_new < 0 || x_new >= w || y_new < 0 || y_new >= h -- lam < -pi || lam > pi
-          then PixelRGB8 0 0 0
-          else pixelAt equirectangular x_new y_new
-      where
-        x = fromToS (-pi) (pi) (fromIntegral xPx / fromIntegral (w-1))
-        y = fromToS (-1) 1 (fromIntegral yPx / fromIntegral (h-1))
-        lam = x
-        phi = asin y
-        x_rect = (((lam + pi) / (2*pi))*fromIntegral w)
-        y_rect = (((phi + pi/2) / (pi))*fromIntegral h)
-        x_new = round (fromToS (fromIntegral xPx) x_rect t)
-        y_new = round (fromToS (fromIntegral yPx) y_rect t)
+    x = fromToS (-pi) (pi) x'
+    y = fromToS (-1) 1 y'
 
-cot = recip . tan
 toRads dec = dec/180 * pi
 
 {- Bottomley
 -}
 
-bottomley :: Double -> Image PixelRGB8
-bottomley phi_1 =
-    generateImage fn w h
+bottomleyP :: Double -> Projection
+bottomleyP phi_1 x' y' = (lam, -phi)
   where
-    w = imageWidth equirectangular
-    h = imageHeight equirectangular
-    fn xPx yPx = -- trace (show (xPx, yPx, lam, phi, x_rect, y_rect)) $
-        if x_new < 0 || x_new >= w || y_new < 0 || y_new >= h -- lam < -pi || lam > pi
-          then PixelRGB8 0 0 0
-          else pixelAt equirectangular x_new y_new
-      where
-        rho = sqrt ((x * sin phi_1)**2 + (pi/2 - y)**2)
-        e = atan2 (x*sin phi_1) (pi/2 - y)
-        x = fromToS (-pi) (pi) (fromIntegral xPx / fromIntegral (w-1))
-        y = fromToS (pi/2) (-pi/2) (fromIntegral yPx / fromIntegral (h-1))
-        lam = (e*rho)/(sin phi_1*sin rho)
-        phi = pi/2 - rho
-        x_rect = (((lam + pi) / (2*pi))*fromIntegral w)
-        y_rect = ((1-(phi + pi/2) / (pi))*fromIntegral h)
-        x_new = round (fromToS (fromIntegral xPx) x_rect 1)
-        y_new = round (fromToS (fromIntegral yPx) y_rect 1)
-
-bottomley_toSphere phi_1 x y = (lam, phi)
-  where
+    x = fromToS (-pi) (pi) x'
+    y = fromToS (pi/2) (-pi/2) y'
     rho = sqrt ((x * sin phi_1)**2 + (pi/2 - y)**2)
     e = atan2 (x*sin phi_1) (pi/2 - y)
     lam = (e*rho)/(sin phi_1*sin rho)
     phi = pi/2 - rho
 
-bottomley_coords phi_1 lam phi = (x,y)
+sinusoidalP :: Projection
+sinusoidalP x' y' = (x/cos y, y)
   where
-    rho = pi/2 - phi
-    e = if rho == 0 then 0 else (lam * sin phi_1 * sin rho) / rho
-    x = (rho * sin e) / sin phi_1
-    y = pi/2 - rho * cos e
+    x = fromToS (-pi) (pi) x'
+    y = fromToS (-pi/2) (pi/2) y'
 
+orthoP :: Double -> Double -> Projection
+orthoP lam_0 phi_0 x' y' = (lam, phi)
+  where
+    x = fromToS (-16/9) (16/9) x'
+    y = fromToS (-1) (1) y'
+    lam = wrap (-pi) (pi) $
+      lam_0 + atan2 (x * sin c) (rho * cos c * cos phi_0 - y * sin c * sin phi_0)
+    phi = wrap (-pi/2) (pi/2) $
+      asin ((cos c * sin phi_0 + y * sin c * cos phi_0)/rho)
+    rho = sqrt (x**2 + y**2)
+    c = asin rho
+    wrap lower upper v
+      | v > upper = v-upper+lower
+      | v < lower = v+upper-lower
+      | otherwise = v
 
 {- Cassini
 phi = asin (sin y * cos x)
