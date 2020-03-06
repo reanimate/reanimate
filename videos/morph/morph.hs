e5ecef5f473d9e6491a1da1699173347f8391863
David Himmelstrup <lemmih@gmail.com>
2020-03-06 17:24:27 +0800

    Use hgeometry.

--- videos/morph/morph.hs
+++ videos/morph/morph.hs
@@ -2,24 +2,38 @@
 -- stack --resolver lts-13.14 runghc --package reanimate
 module Main where
 
+import           Algorithms.Geometry.DelaunayTriangulation.DivideAndConquer (delaunayTriangulation)
+import           Algorithms.Geometry.DelaunayTriangulation.Types
 import           Codec.Picture.Types
 import           Control.Lens
 import           Control.Monad
-import           Data.List                     (delete, nub, partition)
+import           Data.Function
+import           Data.List
+import           Data.Geometry.Point
+import           Data.Ext
+import Data.CircularList as CL
+import           Data.List.NonEmpty                                         (NonEmpty)
+import qualified Data.List.NonEmpty                                         as NE
 import           Data.Maybe
-import qualified Data.Text                     as T
+import qualified Data.Text                                                  as T
 import           Data.Tuple
-import qualified Data.Vector                   as V
+import qualified Data.Vector                                                as V
 import           Debug.Trace
-import           Linear.Matrix                 hiding (trace)
+import           Linear.Matrix                                              hiding
+                                                                             (trace)
 import           Linear.V2
 import           Linear.V3
 import           Linear.Vector
-import           Numeric.LinearAlgebra         hiding (scale, (<>), polar)
-import qualified Numeric.LinearAlgebra         as Matrix
-import           Numeric.LinearAlgebra.HMatrix hiding (scale, (<>), polar)
+import           Numeric.LinearAlgebra                                      hiding
+                                                                             (polar,
+                                                                             scale,
+                                                                             (<>))
+import qualified Numeric.LinearAlgebra                                      as Matrix
+import           Numeric.LinearAlgebra.HMatrix                              hiding
+                                                                             (polar,
+                                                                             scale,
+                                                                             (<>))
 import           Reanimate
-import Data.Function
 
 type Points = V.Vector (V2 Double)
 type Edges = [(Int, Int, Int)]
@@ -45,7 +59,9 @@ data RelMeshPair = RelMeshPair Points Edges (Matrix Double) (Matrix Double) (Mat
 -- convexInterpolate :: RelMeshPair -> Double -> RelMesh
 
 main :: IO ()
-main = reanimate $ playThenReverseA $ pauseAround 1 1 $ addStatic (mkBackground "black") $
+main = reanimate testTrigs
+
+morphAnimation = playThenReverseA $ pauseAround 1 1 $ addStatic (mkBackground "black") $
   signalA (curveS 2) $ mkAnimation 5 $ \t -> lowerTransformations $ scale 3 $ pathify $ center $
   mkGroup
   [ translate 1.7 0 $ drawTrig example1
@@ -55,6 +71,29 @@ main = reanimate $ playThenReverseA $ pauseAround 1 1 $ addStatic (mkBackground 
   ]
   where
     meshPair = fromJust $ compatible example1 example2
+
+testTrigs :: Animation
+testTrigs = addStatic (mkBackground "black") $ animate $ \t ->
+    withStrokeColor "white" $
+    mkGroup
+    [ --translate (-3.5) 0 $ renderTriangulation $ dummyTriangulate points
+     translate (3.5) 0 $ renderTriangulation $ delaunay points
+    ]
+  where
+-- points = [V2 0 0, V2 1 1, V2 4 2, V2 (-2) (-4), V2 (-3) 4]
+points = [V2 0 0, V2 1 1, V2 2 0, V2 3 (-3), V2 (-2) (-1) ]
+         -- ,V2 0 1, V2 1 1, V2 2 1, V2 3 1]
+
+renderTriangulation :: [(P,P)] -> SVG
+renderTriangulation trigs =
+  mkGroup
+  [ mkGroup
+    [ mkLineP a b ]
+  | (a,b) <- trigs
+  ]
+
+mkLineP :: P -> P -> SVG
+mkLineP (V2 x1 y1) (V2 x2 y2) = mkLine (x1,y1) (x2,y2)
 
 -- FIXME: Check that the triangles are all anticlockwise.
 -- FIXME: Check that the edges connect all the points.
@@ -340,7 +379,7 @@ isLeftTurn p1 p2 p3 = direction p1 p2 p3 <= 0
 
 isRightTurn :: P -> P -> P -> Bool
 isRightTurn p1 p2 p3 = not $ isLeftTurn p1 p2 p3
-
+{-
 vispol z ((v0,a0):(v1,a1):rest)
   | isLeftTurn z v0 v1 = left [v1,v0] ((v1,a1):rest)
   | otherwise          = scana [v0] ((v1,a1):rest)
@@ -366,3 +405,25 @@ vispol z ((v0,a0):(v1,a1):rest)
             ... scan_d
       | isForwardMove z sji sj && (vPrev,vi) intersects (sji,sj) = ...
       | otherwise right (sji:ss) ((vi,ai):rest)
+-}
+
+dummyTriangulate :: [P] -> [(P,P,P)]
+dummyTriangulate [a,b,c]    = [(a,b,c)]
+dummyTriangulate (a:b:c:xs) = sortTrig (a,b,c) : dummyTriangulate (b:c:xs)
+dummyTriangulate _          = []
+
+sortTrig :: (P,P,P) -> (P,P,P)
+sortTrig (a,b,c) =
+  case sortOn (polar . subtract center) [a,b,c] of
+    [a',b',c'] -> (a',b',c')
+  where
+    center = (a+b+c) ^/ 3
+
+delaunay :: [P] -> [(P,P)]
+delaunay ps =
+  [ (V2 ax ay, V2 bx by)
+  | (a, b) <- triangulationEdges $ delaunayTriangulation
+        (NE.fromList $ [ ext $ Point2 x y | V2 x y <- ps])
+  , let Point2 ax ay = _core a
+        Point2 bx by = _core b
+  ]
