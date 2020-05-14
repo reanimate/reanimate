1a215ea899f5fd0626a4852dc8c7ea28b42a356e
David Himmelstrup <lemmih@gmail.com>
2020-05-14 10:05:49 +0800

    Expose newSprite_ and newSpriteSVG_

--- src/Reanimate.hs
+++ src/Reanimate.hs
@@ -103,9 +103,11 @@ module Reanimate
   , spriteT           -- :: Frame s Time
   , spriteDuration    -- :: Frame s Duration
   , newSprite         -- :: Frame s SVG -> Scene s (Sprite s)
+  , newSprite_        -- :: Frame s SVG -> Scene s ()
   , newSpriteA        -- :: Animation -> Scene s (Sprite s)
   , newSpriteA'       -- :: Sync -> Animation -> Scene s (Sprite s)
   , newSpriteSVG      -- :: SVG -> Scene s (Sprite s)
+  , newSpriteSVG_     -- :: SVG -> Scene s ()
   , destroySprite     -- :: Sprite s -> Scene s ()
   , applyVar          -- :: Var s a -> Sprite s -> (a -> SVG -> SVG) -> Scene s ()
   , spriteModify      -- :: Sprite s -> Frame s ((SVG,ZIndex) -> (SVG, ZIndex)) -> Scene s ()
