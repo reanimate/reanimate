3e371f76270405bb07d2a07387c7e6b285b1ece1
David Himmelstrup <lemmih@gmail.com>
2020-06-02 20:14:47 +0800

    Narrated animation about color theory (#84)
    * Add slide transitions.
    
    Co-authored-by: eveyyp <55053150+eveyyp@users.noreply.github.com>
    Former-commit-id: afa93f45f8d0f556ad73e349091f0226ce173eed

--- videos/color-theory/posters.hs
+++ videos/color-theory/posters.hs
@@ -1,85 +1,35 @@
 #!/usr/bin/env stack
--- stack --resolver lts-13.14 runghc --package reanimate
+-- stack runghc --package reanimate
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-module Main (main) where
+module Main
+  ( main
+  )
+where
 
-import           Control.Lens          ()
-import           Control.Monad
-import qualified Data.ByteString       as BS
-import qualified Data.Map              as Map
-import           Data.Monoid
-import qualified Data.Text             as T
-
-import           Codec.Picture
-import           Codec.Picture.Jpg
-import           Codec.Picture.Types
-import           Data.Word
-import           Graphics.SvgTree      hiding (Image, imageHeight, imageWidth)
-import           Graphics.SvgTree.Memo
-import           Numeric
-import           Reanimate.ColorMap
-import           Reanimate.Driver      (reanimate)
-import           Reanimate.LaTeX
+import           System.Directory
 import           Reanimate
-import           Reanimate.Animation
-import           Reanimate.Raster
-import           Reanimate.Scene
-import           Reanimate.Ease
-import           Reanimate.Effect
-import           Reanimate.Svg
-import           Reanimate.ColorSpace
-import           Reanimate.Constants
-import           System.IO.Unsafe
+
+import           Common
 
 main :: IO ()
-main = reanimate $
-  mkAnimation (1/60) (const monalisaPoster)
+main = renameFile (svgAsPngFile monalisaPoster) "poster.png"
 
 monalisaPoster :: SVG
-monalisaPoster =
-    mkGroup
-    --[ mkPic (-1) 1 jet,       mkPic 0 1 turbo,      mkPic 1 1 parula
-    --, mkPic (-1) 0 viridis,   mkPic 0 0 inferno,    mkPic 1 0 sinebow
-    --, mkPic (-1) (-1) plasma, mkPic 0 (-1) cividis, mkPic 1 (-1) hsv ]
-
-    [ mkPic (-1) 1 viridis "viridis",   mkPic 0 1 cividis "cividis",    mkPic 1 1 parula "parula"
-    , mkPic (-1) 0 jet "jet",   mkPic 0 0 inferno "inferno",    mkPic 1 0 sinebow "sinebow"
-    , mkPic (-1) (-1) turbo "turbo", mkPic 0 (-1) plasma "plasma", mkPic 1 (-1) hsv "hsv" ]
-  where
-    mkPic x y cm txt =
-      translate (screenWidth/3 * x) (screenHeight/3 * y) $
-      mkGroup
-      [ scaleToSize (screenWidth/3) (screenHeight/3) $ embedImage $
-        applyColorMap cm monalisa
-      , translate (-screenWidth/6) (screenHeight/6) $
-        scale 0.5 $
-        withStrokeColor "black" $
-        withStrokeWidth (defaultStrokeWidth*0.5) $
-        withFillColor "white" $
-        latex ("\\texttt{" <> txt <> "}")
+monalisaPoster = mkGroup
+  [ mkPic (-1) 1    viridis -- "viridis"
+  , mkPic 0    1    cividis -- "cividis"
+  , mkPic 1    1    parula  -- "parula"
+  , mkPic (-1) 0    jet     -- "jet"
+  , mkPic 0    0    inferno -- "inferno"
+  , mkPic 1    0    sinebow -- "sinebow"
+  , mkPic (-1) (-1) turbo   -- "turbo"
+  , mkPic 0    (-1) plasma  -- "plasma"
+  , mkPic 1    (-1) hsv     -- "hsv"
+  ]
+ where
+  mkPic x y cm =
+    translate (screenWidth / 3 * x) (screenHeight / 3 * y) $ mkGroup
+      [ scaleToSize (screenWidth / 3) (screenHeight / 3)
+        $ embedImage
+        $ applyColorMap cm monalisa
       ]
-
-monalisa :: Image PixelRGB8
-monalisa = unsafePerformIO $ do
-  dat <- BS.readFile "monalisa.jpg"
-  case decodeJpeg dat of
-    Left err  -> error err
-    Right img -> return $ convertRGB8 img
-
-monalisaLarge :: Image PixelRGB8
-monalisaLarge = scaleImage 15 monalisa
-
-scaleImage :: Pixel a => Int -> Image a -> Image a
-scaleImage factor img =
-    generateImage fn (imageWidth img * factor) (imageHeight img * factor)
-  where
-    fn x y = pixelAt img (x `div` factor) (y `div` factor)
-
-applyColorMap :: (Double -> PixelRGB8) -> Image PixelRGB8 -> Image PixelRGB8
-applyColorMap cmap img =
-    generateImage fn (imageWidth img) (imageHeight img)
-  where
-    fn x y =
-      case pixelAt img x y of
-        PixelRGB8 r _ _ -> cmap (fromIntegral r/255)
