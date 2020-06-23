8f7b19d8d72d3414e8aeb1d521a1450d2fb62e1b
Ben Ford <ben@perurbis.com>
2020-06-23 18:53:38 +0100

    Add: Fix nix error

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/dc7acfeb7628032d5109747a8b330d58817b953f.tar.gz") {}
+{ haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}
 , nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
 , nixpkgsArgs ? haskellNix.nixpkgsArgs
 , pkgs ? import nixpkgsSrc nixpkgsArgs
@@ -9,4 +9,47 @@ pkgs.haskell-nix.project {
     name = "reanimate";
     src = ./.;
   };
+  modules = [{
+    nonReinstallablePkgs =
+      [ "rts"
+        "ghc-heap"
+        "ghc-prim"
+        "integer-gmp"
+        "integer-simple"
+        "base"
+        "deepseq"
+        "array"
+        "ghc-boot-th"
+        "pretty"
+        "template-haskell"
+        "ghcjs-prim"
+        "ghcjs-th"
+        "ghc-boot"
+        "ghc"
+        "Cabal"
+        "Win32"
+        "array"
+        "binary"
+        "bytestring"
+        "containers"
+        "directory"
+        "filepath"
+        "ghc-boot"
+        "ghc-compact"
+        "ghc-prim"
+        "ghci"
+        "haskeline"
+        "hpc"
+        "mtl"
+        "parsec"
+        "process"
+        "text"
+        "time"
+        "transformers"
+        "unix"
+        "xhtml"
+        "stm"
+        "terminfo"
+      ];
+  }];
 }
