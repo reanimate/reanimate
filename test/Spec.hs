8abe9b4b03754a2ef99994c6c99798f5e6cc2f47
David Himmelstrup <lemmih@gmail.com>
2020-07-11 22:21:36 +0800

    Force use of newer tasty-rerun package.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,7 @@
 module Main (main) where
 
 import           Test.Tasty
-import           Test.Tasty.Ingredients.Rerun (rerunningTests)
-import           Test.Tasty.Runners
+import           Test.Tasty.Ingredients.Rerun (defaultMainWithRerun)
 
 import           Properties
 import           UnitTests
@@ -13,8 +12,3 @@ main = do
   tests2 <- compileTestFolder "examples/"
   tests3 <- compileVideoFolder "videos/"
   defaultMainWithRerun $ testGroup "tests" [tests1, tests2, tests3, all_props]
-
--- Copy of 'defaultMainWithRerun' because that function is not available in lts-12.
-defaultMainWithRerun :: TestTree -> IO ()
-defaultMainWithRerun = defaultMainWithIngredients
-    [ rerunningTests [ listingTests, consoleTestReporter ] ]
