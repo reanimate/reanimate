06b4e8d41765aa499ed58c9c99ae06b045bac946
David Himmelstrup <lemmih@gmail.com>
2020-07-25 13:47:01 +0800

    Add objects.

---
+++ videos/miles/Objects.hs
@@ -0,0 +1,127 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+module Objects where
+
+import           Codec.Picture.Types
+import           Control.Lens
+import           Graphics.SvgTree
+import           Linear.V2
+import           Reanimate
+import           Reanimate.Builtin.CirclePlot
+import           Reanimate.Builtin.Documentation
+import           Reanimate.Scene
+
+
+data RoundFunction = RoundFunction
+  { _roundHeight :: Double
+  , _roundDepth  :: Double
+  }
+
+data SquareFunction = SquareFunction
+  { _squareHeight    :: Double
+  , _squareDepth     :: Double
+  , _squareNubHeight :: Double
+  , _squareNubDepth  :: Double
+  }
+
+data ArcPlot = ArcPlot
+  { _arcPlotPartial :: Double
+  , _arcPlotFn      :: Double -> PixelRGBA8
+  , _arcPlotAngle   :: Double
+  , _arcPlotCenter  :: Double
+  , _arcPlotQuality :: Int
+  }
+-- circlePlot :: Int -- ^ Number of diagonal pixels. Only affects quality, not size.
+--            -> (Double -> Double -> PixelRGBA8)
+--               -- ^ Angle and radius in radians and percent respectively.
+--            -> Tree
+
+
+makeLenses ''RoundFunction
+makeLenses ''SquareFunction
+makeLenses ''ArcPlot
+
+instance Renderable RoundFunction where
+  toSVG (RoundFunction mouthHeight mouthDepth) =
+    mkGroup
+    [ withFillOpacity 0 $ mkPath
+      [ MoveTo OriginAbsolute [V2 (mouthDepth-1) 0]
+      , VerticalTo OriginRelative [-mouthHeight/2]
+      , HorizontalTo OriginAbsolute [-mouthX]
+      , EllipticalArc OriginRelative
+          [(1,1,0,True,True,V2 0 mouthHeight)]
+      , HorizontalTo OriginAbsolute [mouthDepth-1]
+      , VerticalTo OriginRelative [-mouthHeight/2]
+      , EndPath
+      ]
+    ]
+    where
+      mouthX = cos (asin (mouthHeight/2))
+
+instance Renderable SquareFunction where
+  toSVG (SquareFunction mouthHeight mouthDepth nubHeight nubDepth) =
+    mkGroup
+    [ withFillOpacity 0 $ mkPath
+      [ MoveTo OriginAbsolute [V2 (mouthDepth-1) 0]
+      , VerticalTo OriginRelative [-mouthHeight/2]
+      , HorizontalTo OriginAbsolute [-1]
+      , VerticalTo OriginAbsolute [-1]
+      , HorizontalTo OriginAbsolute [1]
+
+      , VerticalTo OriginAbsolute [-nubHeight/2]
+      , HorizontalTo OriginAbsolute [1-nubDepth]
+      , VerticalTo OriginAbsolute [nubHeight/2]
+      , HorizontalTo OriginAbsolute [1]
+
+      , VerticalTo OriginAbsolute [1]
+      , HorizontalTo OriginAbsolute [-1]
+      , VerticalTo OriginAbsolute [mouthHeight/2]
+      , HorizontalTo OriginAbsolute [mouthDepth-1]
+      , VerticalTo OriginRelative [-mouthHeight/2]
+      , EndPath
+      ]
+    ]
+
+instance Renderable ArcPlot where
+  toSVG plot | plot^.arcPlotPartial == 0 = None
+  toSVG plot = mkGroup
+      [ withId "arcPlotMask" $
+        MaskTree $ defaultSvg
+        & maskWidth .~ Num screenWidth
+        & maskHeight .~ Num screenHeight
+        & maskContent .~
+          [ withFillOpacity 1 $
+            withStrokeWidth 0 $
+            mkGroup
+            [ -- Show everything by default
+              withFillColor "white" $
+              mkCircle 1
+              -- Hide center
+            , withFillColor "black" $
+              mkCircle (plot^.arcPlotCenter)
+              -- Hide everything outside of arcPlotAngle
+            , withFillColor "black" $ scale 2 $ mkPath
+              [ MoveTo OriginAbsolute [V2 0 0]
+              , LineTo OriginAbsolute [V2 (cos pAng) (sin pAng)]
+              , EllipticalArc OriginAbsolute
+                [(1,1,0,True,True,V2 (cos pAng) (-sin pAng))]
+              , EndPath ]
+              -- Partially hide arc plot
+            , withFillColor "black" $ mkPath
+              [ MoveTo OriginAbsolute [V2 0 0]
+              , LineTo OriginAbsolute [V2 (-1) (0)]
+              , EllipticalArc OriginAbsolute
+                [(1,1,0,lAng>0,True,V2 (cos lAng) (sin lAng))]
+              , EndPath ]
+            ]
+          ]
+          
+      , (lowerTransformations $
+        circlePlot (plot^.arcPlotQuality) gen)
+        & maskRef .~ pure (Ref "arcPlotMask")
+      ]
+    where
+      pAng = (plot^.arcPlotAngle)/2
+      lAng = fromToS pAng (-pAng) (plot^.arcPlotPartial)
+      gen ang _r =
+        (plot^.arcPlotFn) ((ang/pAng+1)/2)
--- videos/miles/miles.hs
+++ videos/miles/miles.hs
@@ -1,3 +1,64 @@
+#!/usr/bin/env stack
+-- stack runghc --package reanimate
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 module Main where
-main = return ()
 
+import           Codec.Picture.Types
+import           Control.Lens
+import           Graphics.SvgTree                (Origin (..), PathCommand (..))
+import           Linear.V2
+import           Reanimate
+import           Reanimate.Animation
+import           Reanimate.Builtin.Documentation
+import           Reanimate.Scene
+
+import           Objects
+
+env :: Animation -> Animation
+env = mapA (withStrokeColor "black")
+
+main :: IO ()
+main = reanimate $ env $ sceneAnimation $ do
+  newSpriteSVG_ $ mkBackground "grey"
+  aPlot <- newObject $ ArcPlot
+    { _arcPlotPartial = 1
+    , _arcPlotFn      = promotePixel . viridis
+    , _arcPlotAngle   = 120/180*pi
+    , _arcPlotCenter  = 0.5
+    , _arcPlotQuality = 100
+    }
+  rFn <- newObject $ RoundFunction 1 0.5
+  sFn <- newObject $ SquareFunction 1 0.5 0.1 0.5
+  oModify aPlot $
+    oContext %~ \o -> scale 3 . o
+  oModify rFn $
+    oContext %~ \o -> lowerTransformations . scale 3 . o
+
+  oDraw rFn 1
+  oFadeIn aPlot 1
+  -- oShow rFn
+
+  -- oTweenV aPlot 1 $ \t ->
+  --   arcPlotAngle %~ \v -> fromToS v (60/180*pi) t
+  -- oTweenV aPlot 1 $ \t ->
+  --   arcPlotAngle %~ \v -> fromToS v (300/180*pi) t
+  -- oTweenV aPlot 1 $ \t ->
+  --   arcPlotCenter %~ \v -> fromToS v 0.1 t
+  -- oTweenV aPlot 1 $ \t ->
+  --   arcPlotCenter %~ \v -> fromToS v 0.9 t
+
+  -- oTweenV aPlot 3 $ \t ->
+  --   arcPlotPartial %~ \v -> fromToS v 1 t
+  -- oTweenV aPlot 1 $ \t ->
+  --   arcPlotAngle %~ \v -> fromToS v (60/180*pi) t
+  -- oTweenV aPlot 1 $ \t ->
+  --   arcPlotPartial %~ \v -> fromToS v 0 t
+
+  wait 1
+
+oDraw :: Object s a -> Duration -> Scene s ()
+oDraw o d = do
+  oShow o
+  oTweenS o d $ \t ->
+    oContext %= \c -> withFillOpacity (max 0 $ t*10-9) . partialSvg t . c
