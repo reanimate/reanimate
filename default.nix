4622e012cfbce89ae4f8427b9cd9eb2e70a6f1bc
David Himmelstrup <lemmih@gmail.com>
2020-09-26 08:29:57 +0800

    Enable nix.

--- default.nix
+++ default.nix
@@ -1,10 +1,24 @@
 let
-  haskellNixSrc = fetchTarball {
-    url = "https://github.com/input-output-hk/haskell.nix/tarball/403b1a1b848295b907f4ceaeb0b6508c3f97e3a3";
-    sha256 = "1m32hz6zalfhywrzjir4gn81xbwp9vmxqgv3x746gb1zzmjqdhhs";
+  hackageSrc = fetchTarball {
+    url = "https://github.com/input-output-hk/hackage.nix/tarball/f95145a966a728f203c42b45bd7a101d067d5ec5";
+    sha256 = "1grr6l68j1sn79ig6qgj19agqyb24j7iibrim4r6vk0pr9i104za";
+  };
+  # Stackage isn't used right now.
+  stackageSrc = fetchTarball {
+    url = "https://github.com/input-output-hk/stackage.nix/tarball/f319b88ce4d3c0880287a96b9c54626802df1eb5";
+    sha256 = "1shwq0faphycs6b3i2phvj0ram242vh02wmr5h2spxin1lda2995";
+  };
+  haskellSrc = fetchTarball {
+    url = "https://github.com/input-output-hk/haskell.nix/tarball/b406bb1d5bed581f651ae18a5d3ed07f47ace2b9";
+    sha256 = "0lcb5p14097b1ya2flwkcbq65crc3k2fzb376bq8ck84i707shqi";
   };
   # haskellSrcMaster = builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz;
-  haskellNix = import haskellNixSrc {};
+  haskellNix = import haskellSrc {
+    sourcesOverride = {
+      hackage = hackageSrc;
+      stackage = stackageSrc;
+    };
+  };
 
   all-hies = fetchTarball {
     # Insert the desired all-hies commit here
@@ -19,7 +33,7 @@ let
     ];
   });
 
-  set = pkgs.haskell-nix.stackProject {
+  set = pkgs.haskell-nix.cabalProject {
     name = "reanimate";
     compiler-nix-name = "ghc883";
     src = pkgs.haskell-nix.haskellLib.cleanGit {
